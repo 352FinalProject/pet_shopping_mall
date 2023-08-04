d1dfe0e744d735af117ff0cad943c5f56e27b99e
bae <dissjn2@gmail.com>
2023-08-04 15:01:23 +0900

    cc
    cc

---
+++ pet-shopping-mall/src/main/java/com/shop/app/member/controller/MemberController.java
@@ -0,0 +1,18 @@
+package com.shop.app.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Validated
+@RequestMapping("/member")
+@Controller
+public class MemberController {
+
+	@GetMapping("/login.do")
+	public void memberLogin() {}
+}
