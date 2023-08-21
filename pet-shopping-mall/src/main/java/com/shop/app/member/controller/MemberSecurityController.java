adb132a0e75bdcdfcd4d71abd42cdac3a3733a25
전수경 <sookyeong0505@gmail.com>
2023-08-21 12:37:37 +0900

    import 오류 수정

--- pet-shopping-mall/src/main/java/com/shop/app/member/controller/MemberSecurityController.java
+++ pet-shopping-mall/src/main/java/com/shop/app/member/controller/MemberSecurityController.java
@@ -1,14 +1,35 @@
 package com.shop.app.member.controller;
 
+
 import java.util.Collection;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 
-import com.shop.app.member.dto.MailDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import com.shop.app.member.dto.MemberCreateDto;
 import com.shop.app.member.dto.MemberUpdateDto;
 import com.shop.app.member.entity.Member;
@@ -16,9 +37,6 @@ import com.shop.app.member.entity.MemberDetails;
 import com.shop.app.member.service.MemberService;
 import com.shop.app.point.entity.Point;
 import com.shop.app.point.service.PointService;
-import com.shop.app.servicecenter.chat.dto.ChatSession;
-import com.shop.app.terms.entity.Accept;
-import com.shop.app.terms.entity.Terms;
 import com.shop.app.terms.service.TermsService;
 
 import lombok.extern.slf4j.Slf4j;
